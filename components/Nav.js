c3af5332d2603ae61abf970c1fbce149f7fd1247
madarapower <mister.igor33@mailru>
2024-07-20 23:13:33 +0300

    god

--- components/Nav.js
+++ components/Nav.js
@@ -1,18 +1,38 @@
+import { auth } from "@/utils/firebase";
 import Link from "next/link";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function Nav() {
+  const [user, loading] = useAuthState(auth);
   return (
     <nav className="flex justify-between items-center py-10">
       <Link href="/">
         <button className="text-lg font-medium">Creative Minds</button>
       </Link>
       <ul className="flex items-center gap-10">
-        <Link href={"/auth/login"}>
-          <span className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
-            Join Now
-          </span>
-        </Link>
+        {!user && (
+          <Link href={"/auth/login"}>
+            <span className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
+              Join Now
+            </span>
+          </Link>
+        )}
+        {user && (
+          <div className="flex item-center gap-6">
+            <Link href="/post">
+              <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-mg textx-sm">
+                Post
+              </button>
+            </Link>
+            <Link href="/dashboard">
+              <img
+                className="w-12 rounded-full cursor-pointer"
+                src={user.photoURL}
+              />
+            </Link>
+          </div>
+        )}
       </ul>
     </nav>
   );
